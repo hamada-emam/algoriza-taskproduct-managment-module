8915af8dfd0611deb94f89e34c30bc1c6cabdd14
Hamada <hamada.emam.tech@gmail.com>
2024-10-17 01:14:50 +0300

    Init db migrations and models

---
+++ database/migrations/2024_10_16_220142_create_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->foreignIdFor(Category::class, 'parent_id')->nullable()->constrained('categories')->onDelete('SET NULL');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+
+            $table->index('parent_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
---
+++ database/migrations/2024_10_16_220143_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->foreignIdFor(Category::class, 'category_id')->constrained('categories')->cascadeOnDelete();
+            $table->string('tags');
+            $table->string('picture')->nullable();
+            $table->timestamps();
+            $table->index('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
